cc2adc3431b91ef61fcfcc00bc6bfcd6864cdb2f
jikai-senpai <thejasondutys@hotmail.com>
2024-10-11 16:32:20 -0400

    mejorando el armaController

--- app/Http/Controllers/armaController.php
+++ app/Http/Controllers/armaController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Armas;
 
 class armaController extends Controller
 {
     public function index()
     {
-        return 'Obteniendo lista de armas desde el controller';
+        $armas = Armas::all();
+
+        if ($armas->isEmpty()) {
+            $data = [
+                'mensaje' => 'No se encontraron armas',
+                'error' => true
+            ];
+            return response()->json($data, 404);
+        }
+
+        return response()->json($armas, 200);
     }
 }
